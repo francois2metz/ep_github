4bafba4b4498f20014c83023948ca487c41a017b
François de Metz <francois@2metz.fr>
2012-07-03 10:38:34 +0200

    Exclude static and javascripts paths.
    Signed-off-by: François de Metz <francois@stormz.me>

--- ep_github.js
+++ ep_github.js
@@ -1,12 +1,18 @@
 var github = require('connect_auth_github');
 
-var settings = require('../ep_etherpad-lite/node/utils/Settings');
-var exp = require('../ep_etherpad-lite/node_modules/express');
+var settings = require('ep_etherpad-lite/node/utils/Settings');
+var exp = require('ep_etherpad-lite/node_modules/express');
 
 exports.expressConfigure = function(hook_name, args, cb) {
-    args.app.use(github.orgAccess({
-        appId: settings.users.github.appId,
-        appSecret: settings.users.github.appSecret,
-        callback: settings.users.github.callack
-    }, settings.users.github.org));
+    args.app.use(function(req, res, next) {
+        if (req.url.match(/^\/static/) || req.url.match(/^\/javascripts/)) {
+            next();
+        } else {
+            github.orgAccess({
+                appId: settings.users.github.appId,
+                appSecret: settings.users.github.appSecret,
+                callback: settings.users.github.callack
+            }, settings.users.github.org).handle(req, res, next);
+        }
+    });
 }
