05a6c840ac96205f16651464111d83bd2137bb46
François de Metz <francois@2metz.fr>
2015-04-19 19:17:07 +0200

    Fix indent and trailing whitespace.

--- ep_github.js
+++ ep_github.js
@@ -2,28 +2,23 @@ var settings = require('ep_etherpad-lite/node/utils/Settings');
 var githubAuth = require('github-auth');
 var config = {
     organization: settings.users.github.org,
-    autologin: true // This automatically redirects you to github to login. 
+    autologin: true // This automatically redirects you to github to login.
 };
 var gh = githubAuth(settings.users.github.appId,settings.users.github.appSecret, config);
 
 exports.expressConfigure = function(hook_name, args, cb) {
-  var app = args.app;
-  app.get('/login', gh.login);
-  app.use(gh.authenticate);
-  app.use(function(req, res, next) {
-
-    if (req.path.match(/^\/(static|javascripts|pluginfw)/)) {
-      // Don't ask for github auth for static paths
-      next();
-
-    } else {
-
-      // Use Github Auth
-      if (!req.github) return res.send('<a href="/login">Please login</a>');
-      if (!req.github.authenticated) return res.send('You shall not pass');
-      next();
-
-    }
-
-  });
+    var app = args.app;
+    app.get('/login', gh.login);
+    app.use(gh.authenticate);
+    app.use(function(req, res, next) {
+        if (req.path.match(/^\/(static|javascripts|pluginfw)/)) {
+            // Don't ask for github auth for static paths
+            next();
+        } else {
+            // Use Github Auth
+            if (!req.github) return res.send('<a href="/login">Please login</a>');
+            if (!req.github.authenticated) return res.send('You shall not pass');
+            next();
+        }
+    });
 }
