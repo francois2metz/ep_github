54c469150acdbffd423ea5d5c5b61e2e4e4f87c5
John McLear <john@mclear.co.uk>
2015-04-19 11:17:55 +0100

    make it work

--- ep_github.js
+++ ep_github.js
@@ -1,18 +1,30 @@
-var github = require('connect_auth_github');
-
 var settings = require('ep_etherpad-lite/node/utils/Settings');
-var exp = require('ep_etherpad-lite/node_modules/express');
+var githubAuth = require('github-auth');
+var config = {
+    team: 'test',
+    organization: settings.users.github.org,
+    autologin: true // This automatically redirects you to github to login. 
+};
+var gh = githubAuth(settings.users.github.appId,settings.users.github.appSecret, config);
 
 exports.expressConfigure = function(hook_name, args, cb) {
-    args.app.use(function(req, res, next) {
-        if (req.path.match(/^\/(static|javascripts|pluginfw)/)) {
-            next();
-        } else {
-            github.orgAccess({
-                appId: settings.users.github.appId,
-                appSecret: settings.users.github.appSecret,
-                callback: settings.users.github.callback
-            }, settings.users.github.org).handle(req, res, next);
-        }
-    });
+  var app = args.app;
+  app.get('/login', gh.login);
+  app.use(gh.authenticate);
+  app.use(function(req, res, next) {
+
+    if (req.path.match(/^\/(static|javascripts|pluginfw)/)) {
+      // Don't ask for github auth for static paths
+      next();
+
+    } else {
+
+      // Use Github Auth
+      if (!req.github) return res.send('<a href="/login">Please login</a>');
+      if (!req.github.authenticated) return res.send('You shall not pass');
+      next();
+
+    }
+
+  });
 }
